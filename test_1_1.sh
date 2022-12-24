5f3a2452097c473853e290d86cbd3afb2298950f
denx24x <d.belozorov1@gmail.com>
2022-12-24 23:06:55 +0300

    fix

--- test_1_1.sh
+++ test_1_1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-> test_1_1.out
+> "test_1_1.out"
 bash mem.bash &
 while true; do
-	data=$(top -b)
-	echo "$data" | head -n 15 >> test_1_1.out
-	echo "$data" | grep "mem.bash" >> test_1_1.out
+	data=$(top -b -n 1)
+	echo "$data" | head -n 15 >> "test_1_1.out"
+	echo "$data" | grep "mem.bash" >> "test_1_1.out"
+	sleep 2s
 done
 
--- test_1_2.sh
+++ test_1_2.sh
@@ -2,14 +2,16 @@
 bash mem.bash &
 bash mem2.bash &
 
-> test_1_2_out
+> "test_1_2_out"
 
 while true; do
-	data=$(top -b)
-	echo "$data" | head -n 15 >> test_1_2_out
-	echo "$data" | grep "mem.bash" >> test_1_2_out
-	echo "$data" | grep "mem2.bash" >> test_1_2_out
+	data=$(top -b -n 1)
+	echo "$data" | head -n 15 >> "test_1_2_out"
+	echo "$data" | grep "mem.bash" >> "test_1_2_out"
+	echo "$data" | grep "mem2.bash" >> "test_1_2_out"
+	sleep 2s
 done
-dmesg | grep "mem[2]*.bash" > test_1_2_log
-cat "report.log" | tail -n 1 >> test_1_2_output
-cat "report2.log" | tail -n 1 >> test_1_2_output
+#
+dmesg | grep "mem[2]*.bash" > "test_1_2_log"
+cat "report.log" | tail -n 1 >> "test_1_2_output"
+cat "report2.log" | tail -n 1 >> "test_1_2_output"
